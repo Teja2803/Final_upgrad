db88ece97bdafb050425cd51cb803419c71f47cd
Teja2803 <tejaswini055@gmail.com>
2025-10-28 21:37:08 +0530

    ansible

--- Task1/azure/azure.tf
+++ Task1/azure/azure.tf
@@ -110,7 +110,7 @@ resource "azurerm_virtual_machine" "app_vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "24.04-LTS"
     version   = "latest"
   }
 
@@ -166,7 +166,7 @@ resource "azurerm_virtual_machine" "tools_vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "24.04-LTS"
     version   = "latest"
   }
 
